f779843d7fd451dcd1847f84c1285e6e6ceb9197
Avin Chadee <achadee15@gmail.com>
2020-01-24 10:33:04 +0900

    fix to_i string parsing error

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,7 +4,7 @@ class EntriesController < ApplicationController
   # root method for displaying all the data locally
   #
   def index
-    page_size = params[:page_size].to_i || 100
+    params[:page_size].to_i == 0 ? page_size = 100 : page_size = params[:page_size].to_i
     page = params[:page].to_i || 0
 
     data = Entry.order("created_on_contentful_at desc").offset(page_size*page).limit(page_size).pluck(:data)
