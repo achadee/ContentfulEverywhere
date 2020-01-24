23a6df586047cce8be16637c9fea83c60cd6d2fc
Avin Chadee <achadee15@gmail.com>
2020-01-24 14:23:36 +0900

    added flexibility to setup.sh for container prefixes

--- setup.sh
+++ setup.sh
@@ -2,9 +2,21 @@
 docker-compose build
 docker-compose up -d
 sleep 5s # Waits 5 seconds because it might take time for daemon to start up.
-docker exec -it contentfuleverywhere_web_1 rake db:setup
+
+# because docker can name containers with prefixes, we search for the container
+# name by the keyword
+web="$(docker ps --format '{{.Names}}' | grep 'web')"
+db="$(docker ps --format '{{.Names}}' | grep 'db')"
+clockwork="$(docker ps --format '{{.Names}}' | grep 'clockwork')"
+
+
+# if you have issues with the database not creating run this line
+# eg. docker exec -it contentfuleverywhere_web_1 rake db:setup
+docker exec -it "${web}" rake db:setup
+
+
 # stop the services
-docker stop contentfuleverywhere_web_1
-docker stop contentfuleverywhere_db_1
-docker stop contentfuleverywhere_clockwork_1
+docker stop "${web}"
+docker stop "${db}"
+docker stop "${clockwork}"
 echo "Contentful everwhere is now setup, you can start the service by running the 'docker-compose up -d' command"
