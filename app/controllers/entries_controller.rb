b3abc2270eb7c7c561b3c41d45bec6aec2d5a399
Avin Chadee <achadee15@gmail.com>
2020-01-24 11:11:56 +0900

    add delete functionality

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -20,4 +20,31 @@ class EntriesController < ApplicationController
     }
     render json: JSON.pretty_generate(response)
   end
+
+
+  # not really restful but will act as our sync endpoint to create and update
+  # new entries
+  #
+  def create
+    # Get the last sucessful sync
+    last_sync = SyncLog.where.not(delta_token: nil).last
+
+    # grab the sync_id
+    #
+    sync_id = last_sync.id if last_sync
+
+    # this is a manual sync so lets perform it syncronously so there is feedback
+    #  for the user
+    SyncContentJob.perform_now sync_id if !sync_id || last_sync.finished?
+    redirect_to "/entries"
+  end
+
+  def destroy
+    # clean up the entries and the sync logs
+    Entry.destroy_all
+    SyncLog.destroy_all
+
+    # redirect to an empty entries page
+    redirect_to "/entries"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+
+  # a hack so I dont have to write JS and can use HTML forms :^)
+  match 'delete_data' => 'entries#destroy', :via => :post
+
+  # entries resource
   resources :entries
 end
